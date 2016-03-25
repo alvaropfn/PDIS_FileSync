55ff8f6831ddadfd49db40a94ed6e247fedb93c5
Bledson <bledson@gmail.com>
2016-03-25 02:31:36 -0300

    initial credentials manager

--- client.py
+++ client.py
@@ -6,7 +6,7 @@ class Client:
     def __init__(self, host, port):
         self.SOCKET = socket.socket()
         self.SOCKET.connect((host, port))
-        print(self.SOCKET.recv(1024))
         time.sleep(30)
+        print(self.SOCKET.recv(1024))
 
 client = Client(socket.gethostname(), 9990)
