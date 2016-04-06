e17dfeb09e9868415c7f6cf86714436ee3d5abc4
João Matias Pereira Neto <jmatiasn@gmail.com>
2016-04-06 14:10:46 -0300

    Aprimoramento do BuscadorDeDiretorios e ComparadorDeArquivos

--- BuscadorDeDiretorios.py
+++ BuscadorDeDiretorios.py
@@ -1,54 +1,48 @@
-# Primeira implementacao
-# from os import listdir
-# for f in listdir("/home/jmatiasn/Git/PDIS_FileSync/"):
-#     print(f)
-
-# from urllib import error
-
 import os, threading, time
 
 #myPath = input("entre com o caminho a ser pesquisado")
-myPath = "/home/jmatiasn/Git/"
+myPath = "/home/jmatiasn/Git/PDIS_FileSync"
 
 class searcherThread(threading.Thread):
-	def __init__(self, path, id = 0):
-		threading.Thread.__init__(self)
-		self.path = path
-		self.id = id
 
-	def run(self):
-		try:
-			self.pathSearch(self.path)
-		except :
-			print("erro")
+    def __init__(self, path, id = 0):
+        threading.Thread.__init__(self)
+        self.path = path
+        self.id = id
 
-	def pathSearch(self, oldPath):
-		#create new threads to start search in subdirectories
-		for dir in listDirs(oldPath):
-			newPath = os.path.join(oldPath, dir)
-			#self._tstate_lock()
-			id = self.id + 1
+    def run(self):
+        try:
+            self.pathSearch(self.path)
+        except :
+            print("erro na execução do método pathSearch")
 
-			try:
-				nt = searcherThread(newPath, id)
-				nt.start()
-			except:
-				print("erro!")
+    def pathSearch(self, oldPath):
+        #create new threads to start search in subdirectories
+        for dir in listDirs(oldPath):
+            newPath = os.path.join(oldPath, dir)
+            #self._tstate_lock()
+            id = self.id+1
 
-        #print all folders in this directory
-		for folder in listDirs(oldPath):
-			print(self.getName(), os.path.join(oldPath, folder))
+            try:
+                nt = searcherThread(newPath, id)
+                nt.start()
+            except:
+                print("erro na execução do método searcherThread")
 
-        #print all files in this directory
-		for file in listFiles(oldPath):
-			print(self.getName(), os.path.join(oldPath, file))
+                #print all folders in this directory
+        # for folder in listDirs(oldPath):
+            # print(os.path.join(oldPath, folder))
+
+            #print all files in this directory
+        for file in listFiles(oldPath):
+            print(os.path.join(oldPath, file))
 
 '''searcherThread ends'''
 ########################################################
-listDirs = lambda path: [d for d in os.listdir(path)
-                         if os.path.isdir(d)]
-listFiles = lambda path: [f for f in os.listdir(path)
-                          if not os.path.isdir(f)]
+listDirs = lambda path: [d.name for d in os.scandir(path)
+                         if d.is_dir()]
+listFiles = lambda path: [f.name for f in os.scandir(path)
+                          if f.is_file()]
 
 ps = searcherThread(myPath)
 ps.start()
--- ComparadorDeArquivos.py
+++ ComparadorDeArquivos.py
@@ -1,19 +1,8 @@
-# Primeira implementacao
-# fileName = "/home/jmatiasn/Git/PDIS_FileSync/BuscadorDeDiretorios.py"
-# statbuf = os.stat(fileName)
-# print "Buscador"
-# print "Modification time:",statbuf.st_mtime
-
-# Segunda implementacao
-# import os.path, time
-# filename = "/home/jmatiasn/Git/PDIS_FileSync/BuscadorDeDiretorios.py"
-# print "last modified: %s" % time.ctime(os.path.getmtime(filename))
-
 import os
 
 #myPath = input("entre com o caminho a ser pesquisado")
-myPaths = ["/home/jmatiasn/Git/PDIS_FileSync/BuscadorDeDiretorios.py",
-                        "/home/jmatiasn/Git/PDIS_FileSync/ComparadorDeArquivos.py"]
+myPaths = ["/home/jmatiasn/Git/",
+           "/home/jmatiasn/Downloads/multcast/"]
 
 # recebe um array com 2 strings de paths de arquivos
 class ComparadorDeArquivos(object):
@@ -21,14 +10,22 @@ class ComparadorDeArquivos(object):
     def __init__(self, filepaths):
         self.filePath1 = filepaths[0]
         self.filePath2 = filepaths[1]
-        self.statbuf1 = os.stat(self.filePath1).st_mtime
-        self.statbuf2 = os.stat(self.filePath2).st_mtime
+        try:
+            self.statbuf1 = os.stat(self.filePath1).st_mtime
+            print(self.statbuf1)
+        except:
+            print("Arquivo1 não encontrado")
+        try:
+            self.statbuf2 = os.stat(self.filePath2).st_mtime
+            print(self.statbuf2)
+        except:
+            print("Arquivo2 não encontrado")
 
     def run(self):
         try:
             self.compararMaisRecente(self.statbuf1, self.statbuf2)
         except :
-            print("erro")
+            print("erro ao comparar 2 caminhos")
 
     def compararMaisRecente(self, statbuf1, statbuf2):
         if statbuf1 > statbuf2:
